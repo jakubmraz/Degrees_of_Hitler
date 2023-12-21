e17b9c70650088d07801911b8ffc9fa68571e214
jakubmraz <jakub.mraz111@gmail.com>
2023-12-21 01:29:13 +0100

    .

--- Python.py
+++ Python.py
@@ -1,40 +1,36 @@
-def calculate_clustering_coefficient(adjacency_list_file):
-    with open(adjacency_list_file, 'r') as file:
-        # Process each line to create a dictionary with node id as key and neighbors as values
-        adjacency_dict = {}
+def load_node_names(file_path):
+    """
+    Loads the node names from the given file.
+    The file format is assumed to be: id name
+    """
+    node_names = {}
+    with open(file_path, 'r') as file:
         for line in file:
-            parts = line.strip().split(':')
-            if len(parts) == 2:
-                node = int(parts[0])
-                neighbors = parts[1].split()
-                adjacency_dict[node] = neighbors
+            parts = line.strip().split(' ')
+            node_id = int(parts[0])
+            node_name = ' '.join(parts[1:])
+            node_names[node_id] = node_name
+    return node_names
 
-    # Function to calculate the clustering coefficient of a single node
-    def clustering_coefficient(node, neighbors):
-        if len(neighbors) < 2:
-            return 0  # No edges possible between less than 2 nodes
+def save_hitler_community_nodes(names_file, community_file, output_file):
+    """
+    Saves the node IDs and corresponding names for nodes in the Hitler community to an output file.
+    """
+    # Load the node names
+    node_names = load_node_names(names_file)
 
-        actual_edges = 0
-        for neighbor in neighbors:
-            # Check for connections between the neighbors
-            neighbor_neighbors = adjacency_dict.get(int(neighbor), [])
-            actual_edges += len([n for n in neighbor_neighbors if n in neighbors])
+    # Open the output file for writing
+    with open(output_file, 'w') as output:
+        # Process the community file
+        with open(community_file, 'r') as file:
+            for line in file:
+                parts = line.strip().split(' ')
+                node_id = int(parts[0])
+                # Write the node ID and its corresponding name to the output file
+                output.write(f'Node ID: {node_id}, Name: {node_names.get(node_id, "Unknown")}\n')
 
-        possible_edges = len(neighbors) * (len(neighbors) - 1)
-        return actual_edges / possible_edges
-
-    # Calculate clustering coefficient for each node
-    coefficients = []
-    for node, neighbors in adjacency_dict.items():
-        coeff = clustering_coefficient(node, neighbors)
-        coefficients.append(coeff)
-
-    # Average clustering coefficient
-    average_coefficient = sum(coefficients) / len(coefficients)
-    return average_coefficient
-
-# Replace this with the path to your adjacency list file
-adjacency_list_file_path = 'filtered_adjacency_list.txt'
-
-average_clustering_coefficient = calculate_clustering_coefficient(adjacency_list_file_path)
-print(f"The average clustering coefficient of the network is: {average_clustering_coefficient}")
+# Example usage
+names_file = r'C:\Users\knedl\Desktop\wiki-topcats-page-names.txt'
+community_file = 'community_thingy//module_18_output.txt'
+output_file = 'hitler_community_nodes.txt'
+save_hitler_community_nodes(names_file, community_file, output_file)
